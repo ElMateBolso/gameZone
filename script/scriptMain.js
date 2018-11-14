69ec3538002041643b4c9d14e68da44d75c1af53
Mateo Fratti <matematefratti@gmail.com>
2018-11-14 16:47:55 -0300

    Add files via upload

--- script/scriptMain.js
+++ script/scriptMain.js
@@ -7,7 +7,8 @@ fetch ("http://localhost/datos/jsonDeJuegos.json")
     var top = document.querySelector(".tops");
     var topP = document.querySelector(".toppagos");
     var topG = document.querySelector(".topgratis");
-    var clase = "impar"
+    var contenedor = clase;
+    var clase = "impar";
     for(a =0; a <b.length; a++){
         if (b[a].variable ===1){
             var clase = "par"
@@ -17,10 +18,10 @@ fetch ("http://localhost/datos/jsonDeJuegos.json")
         }
 
         if(b[a].precio !== "Gratis" && b[a].top === true){
-        topP.innerHTML +="<div class='" + clase +"'><h3 class='titulo'>" + b[a].titulo +"</h3><img src='" + b[a].imagen +"' class='portada'><p class=''>" + b[a].descripcionBreve +"</p></div>";
+        topP.innerHTML +="<div class='" + clase +"'><a href='game1.html'><h3 class='titulo'>" + b[a].titulo +"</h3><p><img src='" + b[a].imagen +"' class='portada'></a>" + b[a].descripcionBreve +"<span class='categoria'>Categoría: " + b[a].categoria +"</span></p></div>";
     }
         else if(b[a].precio ==="Gratis" && b[a].top === true){
-            topG.innerHTML +="<div class='" + clase +"'><h3 class='titulo'>" + b[a].titulo +"</h3><img src='" + b[a].imagen +"' class='portada'></div>";
+            topG.innerHTML +="<div class='" + clase +"'><h3 class='titulo'>" + b[a].titulo +"</h3><p><img src='" + b[a].imagen +"' class='portada'>" + b[a].descripcionBreve +"</div>";
         }
     }
 })
